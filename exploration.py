f595d620a2ee6eb0354868d3b62616c292bafa86
Michael Deuchert <michael.deuchert@online.de>
2019-11-30 21:17:58 +0100

    Added Plots for Stats by Castle

--- exploration.py
+++ exploration.py
@@ -34,7 +34,7 @@ num_cols = [x for x in data.columns if data[x].dtype != "object"]
 str_cols = [x for x in data.columns if data[x].dtype == "object"]
 
 
-# analysis by castle ########################################################
+# analysis by castle
 num_cols_castle = num_cols
 num_cols_castle.append("Castle")
 data_castle = data[reversed(num_cols_castle)]
@@ -44,3 +44,29 @@ data_castle_grouped = data_castle.groupby("Castle").sum()
 
 # remove castle "neutral" since this castle not playable
 data_castle_grouped.drop("Neutral", inplace=True)
+
+data_castle_grouped.drop("AI_Value", axis=1, inplace=True)
+
+# plot horizontal barcharts for each column
+for idx, col in enumerate(data_castle_grouped.columns):
+    # plot figure object
+    fig = plt.figure(num=0)
+    fig.set_size_inches(18.5, 10.5, forward=True)
+    fig.tight_layout()
+    ax = fig.add_subplot(2, 4, idx + 1)
+
+    # sorted values
+    sorted_col = data_castle_grouped[col].sort_values()
+
+    # create bar plot
+    ax.barh(sorted_col.index, sorted_col.values)
+
+    plt.title(f"Feature: {col}")
+
+# save plot
+fig.savefig(
+        r"plots/stats_by_castle.png",
+        dpi=100,
+        bbox_inches="tight",
+        pad_inches=0,
+    )
